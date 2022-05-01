25cf46319b7528e7c01f564a1b873afd9063c509
richardhaynes <rahayn@yahoo.com>
2022-05-01 15:47:52 -0400

    adding cache extensions

--- BigCleopatraBurgers/BigCleopatraBurgers/Services/ProductMenuService.cs
+++ BigCleopatraBurgers/BigCleopatraBurgers/Services/ProductMenuService.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
 using BigCleopatraBurgers.Models;
 using BigCleopatraBurgers.Repositories;
 using BigCleopatraBurgers.Extensions;
+using StackExchange.Redis;
 
 namespace BigCleopatraBurgers.Services
 {
@@ -12,17 +14,30 @@ namespace BigCleopatraBurgers.Services
     {
         private readonly IProductRepository productRepository;
         private readonly IDistributedCache distributedCache;
-        public ProductMenuService(IProductRepository productRepository, IDistributedCache distributedCache)
+        private readonly ILogger<ProductMenuService> logger;
+        public ProductMenuService(IProductRepository productRepository
+            , IDistributedCache distributedCache
+            , ILogger<ProductMenuService> logger)
         {
             this.productRepository = productRepository;
             this.distributedCache = distributedCache;
+            this.logger = logger;
         }
 
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {
             IEnumerable<Product> products;
             string key = $"{nameof(ProductMenuService)}_{DateTime.UtcNow.Hour}";
-            products = await distributedCache.GetValuesAsync<IEnumerable<Product>>(keyId: key);
+            try
+            {
+                products = await distributedCache.GetValuesAsync<IEnumerable<Product>>(keyId: key);
+            }
+            catch(RedisConnectionException ex)
+            {
+                logger.LogWarning($"Redis Cache Unavaliable: {ex.Message}");
+                return await productRepository.GetProductsAsync();
+            }
+            
             if(products is null)
             {
                 products = await productRepository.GetProductsAsync();
